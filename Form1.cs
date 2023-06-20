6623d40dc126d2f46077594985ec0c78ec4da89f
autologon <autologon@RS01-LABH223-04>
2023-06-19 21:57:54 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace campoMinado
+{
+    public partial class FrmCampoMinado : Form
+    {
+        public FrmCampoMinado()
+        {
+            InitializeComponent();
+            Square sq;
+
+            //i é coluna
+            //y é lina
+
+            List<Square> squares = new List<Square>();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    sq = new Square(i, y);
+                    sq.cimaEsquerda = new Square(i - 1, y - 1);
+                    sq.cima = new Square(i, y - 1);
+                    sq.cimaDireita = new Square(i - 1, y);
+                    sq.baixo = new Square(i, y + 1);
+                    sq.baixoDireita = new Square(i + 1, y + 1);
+                    sq.baixoEsquerda = new Square(i - 1, y + 1);
+                    sq.esquarda = new Square(i - 1, y);
+                    sq.baixoDireita = new Square(i + 1, y);
+
+                    if (i == 0)
+                    {
+                        sq.cimaEsquerda = null;
+                        sq.cima = null;
+                        sq.cimaDireita = null;
+                    }
+                    if (i == 9)
+                    {
+                        sq.baixo = null;
+                        sq.baixoDireita = null;
+                        sq.baixoEsquerda = null;
+                    }
+                    if (y == 0)
+                    {
+                        sq.cimaDireita = null;
+                        sq.cima = null;
+                        sq.cimaEsquerda = null;
+                    }
+                    if (y == 9)
+                    {
+                        sq.baixo = null;
+                        sq.baixoDireita = null;
+                        sq.baixoEsquerda = null;
+                    }
+
+                    squares.Add(sq);
+                }
+            }
+        }
+
+        public List<Square> generateBombs(List<Square> listSquare, int qtdBombas)
+        {
+
+            for (int i = 0; i < qtdBombas; i++)
+            {
+
+            }
+            return listSquare;
+        }
+        private void FrmCampoMinado_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campoMinado
+{
+    public class Square
+    {
+        public  int i {get; set;}
+        public  int y {get; set;}
+        public  Boolean bomb { get; set; }
+        public int quantBombs { get; set; }
+        public Square cima { get; set; }
+        public Square direira { get; set; }
+        public Square baixo { get; set; }
+        public Square esquarda { get; set; }
+        public Square cimaDireita { get; set; }
+        public Square cimaEsquerda { get; set; }
+        public Square baixoDireita { get; set; }
+        public Square baixoEsquerda { get; set; }
+        public Square(int i, int y)
+        {
+            this.i = i;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"i:  {i}  y: {y}" ;
+        }
+    }
+}
