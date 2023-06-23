df48f8ab87467b4b3f3ddba308ccbb54511912a6
Alexsander <alexdonay@gmail.com>
2023-06-23 12:59:06 -0300

    quase pronto

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,22 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtColunas = new System.Windows.Forms.TextBox();
+            this.txtLinhas = new System.Windows.Forms.TextBox();
+            this.txtBombas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.txtTempo = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtColunas
             // 
-            this.textBox1.Location = new System.Drawing.Point(95, 135);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(604, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtColunas.Location = new System.Drawing.Point(80, 29);
+            this.txtColunas.Name = "txtColunas";
+            this.txtColunas.Size = new System.Drawing.Size(114, 20);
+            this.txtColunas.TabIndex = 0;
+            // 
+            // txtLinhas
+            // 
+            this.txtLinhas.Location = new System.Drawing.Point(256, 30);
+            this.txtLinhas.Name = "txtLinhas";
+            this.txtLinhas.Size = new System.Drawing.Size(114, 20);
+            this.txtLinhas.TabIndex = 1;
+            // 
+            // txtBombas
+            // 
+            this.txtBombas.Location = new System.Drawing.Point(444, 34);
+            this.txtBombas.Name = "txtBombas";
+            this.txtBombas.Size = new System.Drawing.Size(114, 20);
+            this.txtBombas.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Colunas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Linhas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bombas";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(573, 32);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 6;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Location = new System.Drawing.Point(669, 33);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(100, 20);
+            this.txtTempo.TabIndex = 7;
             // 
             // FrmCampoMinado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtTempo);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBombas);
+            this.Controls.Add(this.txtLinhas);
+            this.Controls.Add(this.txtColunas);
             this.Name = "FrmCampoMinado";
             this.Text = "Campo Minado";
             this.Load += new System.EventHandler(this.FrmCampoMinado_Load);
@@ -54,7 +126,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtColunas;
+        private System.Windows.Forms.TextBox txtLinhas;
+        private System.Windows.Forms.TextBox txtBombas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox txtTempo;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,81 +2,209 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static campoMinado.Square;
 
 namespace campoMinado
 {
     public partial class FrmCampoMinado : Form
     {
+        private Square[,] tabuleiro;
+        private int quantLinhas;
+        private int quantColunas;
+        private int quantBombas;
+        private Panel panelBoard;
+        private int tempoDecorrido;
+        private Timer timerTempo;
         public FrmCampoMinado()
         {
             InitializeComponent();
-            Square sq;
+            timerTempo = new Timer();
+            timerTempo.Interval = 1000;
+        }
 
-            //i é coluna
-            //y é lina
 
-            List<Square> squares = new List<Square>();
-            for (int i = 0; i < 10; i++)
+        private void FrmCampoMinado_Load(object sender, EventArgs e)
+        {
+            panelBoard = new Panel();
+            panelBoard.Location = new Point(10, 100);
+            panelBoard.AutoSize = true;
+            Controls.Add(panelBoard);
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            quantLinhas = int.Parse(txtLinhas.Text);
+            quantColunas = int.Parse(txtColunas.Text);
+            quantBombas = int.Parse(txtBombas.Text);
+            tempoDecorrido = 0;
+            timerTempo.Start();
+            timerTempo.Tick += TimerTempo_Tick;
+            InitializeGame();
+
+        }
+
+        private void TimerTempo_Tick(object sender, EventArgs e)
+        {
+            tempoDecorrido++;
+            txtTempo.Text = TimeSpan.FromSeconds(tempoDecorrido).ToString(@"hh\:mm\:ss");
+            timerTempo.Start();
+           
+        }
+        private void InitializeGame()
+        {
+            Square square = new Square();
+            tabuleiro = square.initGame(quantLinhas, quantColunas, quantBombas);
+            CreateBoardUI();
+        }
+        private void CreateBoardUI()
+        {
+            panelBoard.Controls.Clear();
+            panelBoard.Size = new Size(quantColunas * 30, quantLinhas * 30);
+            for (int i = 0; i < quantLinhas; i++)
             {
-                for (int y = 0; y < 10; y++)
+                for (int j = 0; j < quantColunas; j++)
                 {
-                    sq = new Square(i, y);
-                    sq.cimaEsquerda = new Square(i - 1, y - 1);
-                    sq.cima = new Square(i, y - 1);
-                    sq.cimaDireita = new Square(i - 1, y);
-                    sq.baixo = new Square(i, y + 1);
-                    sq.baixoDireita = new Square(i + 1, y + 1);
-                    sq.baixoEsquerda = new Square(i - 1, y + 1);
-                    sq.esquarda = new Square(i - 1, y);
-                    sq.baixoDireita = new Square(i + 1, y);
+                    Button button = new Button();
+                    button.Size = new Size(30, 30);
+                    button.Location = new Point(j * 30, i * 30);
+                    button.Tag = tabuleiro[i, j];
+                    button.Click += Cell_Click;
+                    button.MouseDown += cell_RightClick;
+                    panelBoard.Controls.Add(button);
+                }
+            }
 
-                    if (i == 0)
-                    {
-                        sq.cimaEsquerda = null;
-                        sq.cima = null;
-                        sq.cimaDireita = null;
-                    }
-                    if (i == 9)
-                    {
-                        sq.baixo = null;
-                        sq.baixoDireita = null;
-                        sq.baixoEsquerda = null;
-                    }
-                    if (y == 0)
-                    {
-                        sq.cimaDireita = null;
-                        sq.cima = null;
-                        sq.cimaEsquerda = null;
-                    }
-                    if (y == 9)
-                    {
-                        sq.baixo = null;
-                        sq.baixoDireita = null;
-                        sq.baixoEsquerda = null;
-                    }
+        }
+        private void cell_RightClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Button button = (Button)sender;
+                Square cell = (Square)button.Tag;
+                button.Text = "F";
+                
+                cell.status = SquareStatus.Flagged;
+            }
+                
+        }
 
-                    squares.Add(sq);
+        private void formLoad()
+        {
+
+        }
+        private void Cell_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Square cell = (Square)button.Tag;
+            if (cell.bomb)
+            {
+                ShowAllBombs();
+                MessageBox.Show("Você perdeu!");
+                
+                InitializeGame();
+            }
+            else
+            {
+                button.Text = cell.quantBombs.ToString();
+                button.Enabled = false;
+                cell.status = SquareStatus.Revealed;
+                
+                if (cell.quantBombs == 0)
+                {
+                    OpenNeighborCells(cell, button);
+                }
+            }
+            if (winGame(tabuleiro))
+            {
+                MessageBox.Show("Voce ganhou");
+            }
+        }
+
+        private void OpenNeighborCells(Square cell, Button button)
+        {
+            if (cell.quantBombs == 0 && cell.bomb == false)
+            {
+                cell.status = SquareStatus.Revealed;
+                button.Enabled = false;
+
+                foreach (Square vizinho in cell.vizinhos)
+                {
+                    if (vizinho.status == SquareStatus.Hidden)
+                    {
+                        Button neighborButton = GetButtonByCell(vizinho);
+                        Cell_Click(neighborButton, EventArgs.Empty);
+                    }
                 }
             }
         }
 
-        public List<Square> generateBombs(List<Square> listSquare, int qtdBombas)
+        private Button GetButtonByCell(Square cell)
         {
-
-            for (int i = 0; i < qtdBombas; i++)
+            foreach (Button button in panelBoard.Controls.OfType<Button>())
             {
+                if (button.Tag == cell)
+                {
+                    return button;
+                }
+            }
+            return null;
+        }
+        private Boolean winGame(Square[,] board)
+        {
+            int quantBombas = 0;
+            int quantHidden = 0;
+            for(int i = 0; i< board.GetLength(0); i++)
+            {
+                for(int y = 0; y< board.GetLength(1); y++)
+                {
+                    if (board[i,y].bomb == true)
+                    {
+                        quantBombas= quantBombas + 1;
+                    }
+                    if (board[i,y].status == SquareStatus.Hidden)
+                    {
+                        quantHidden = quantHidden + 1;
+                    }
+                    
+                }
+            }
+            
+            if(quantHidden == quantBombas)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void ShowAllBombs()
+        {
+            foreach (Button button in panelBoard.Controls.OfType<Button>())
+            {
+                Square cell = (Square)button.Tag;
+                if (cell.bomb)
+                {
+                    button.Text = "B";
+                    button.Enabled = false;
+                    button.BackColor = Color.Red;
+                }
+                else
+                {
+                    button.Text = cell.quantBombs.ToString();
+                    button.Enabled = false;
+                    cell.status = SquareStatus.Revealed;
+
+                }
 
             }
-            return listSquare;
         }
-        private void FrmCampoMinado_Load(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
